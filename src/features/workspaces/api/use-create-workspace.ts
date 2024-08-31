a2c78912221d0fa78b5ec77c67720aad4a9f1a94
ivandjoh <ivandi.djoh@gmail.com>
2024-08-31 10:09:38 +0800

    make small changes

--- src/features/workspaces/api/use-create-workspace.ts
+++ src/features/workspaces/api/use-create-workspace.ts
@@ -1,10 +1,10 @@
-import { useCallback } from "react";
+import { useCallback, useMemo, useState } from "react";
 import { useMutation } from "convex/react";
 import { api } from "../../../../convex/_generated/api";
 import { Doc, Id } from "../../../../convex/_generated/dataModel";
 
 type RequestType = { name: string };
-type ResponseType = Id<"workspaces">;
+type ResponseType = Id<"workspaces"> | null;
 type Options = {
   onSuccess?: (data: ResponseType) => void;
   onError?: (error: Error) => void;
@@ -13,10 +13,27 @@ type Options = {
 };
 
 export const useCreateWorkspace = (options: Options = {}) => {
+  const [data, setData] = useState<ResponseType>(null);
+  const [error, setError] = useState<Error | null>(null);
+
+  const [status, setStatus] = useState<
+    "success" | "error" | "settled" | "pending" | null
+  >(null);
+
+  const isPending = useMemo(() => status === "pending", [status]);
+  const isSuccess = useMemo(() => status === "success", [status]);
+  const isError = useMemo(() => status === "error", [status]);
+  const isSettled = useMemo(() => status === "settled", [status]);
+
   const mutation = useMutation(api.workspaces.create);
   const mutate = useCallback(
     async (values: RequestType, options?: Options) => {
       try {
+        // Set States
+        setData(null);
+        setError(null);
+        setStatus("pending");
+
         const response = await mutation(values);
         options?.onSuccess?.(response);
         return response;
@@ -26,11 +43,12 @@ export const useCreateWorkspace = (options: Options = {}) => {
           throw error;
         }
       } finally {
+        setStatus("settled");
         options?.onSettled?.();
       }
     },
     [mutation],
   );
 
-  return mutate;
+  return { mutate, data, error, isPending, isSuccess, isError, isSettled };
 };
