619fb49882401b67f31f0c105a0d7ee58fd90615
ivandjoh <ivandi.djoh@gmail.com>
2024-09-01 13:23:20 +0800

    modified left sidebar

--- src/app/workspace/[workspaceId]/page.tsx
+++ src/app/workspace/[workspaceId]/page.tsx
@@ -1,22 +1,9 @@
 "use client";
 
-import { useWorkspaceId } from "@/hooks/use-workspace-id";
-import { useGetWorkspace } from "@/features/workspaces/api/use-get-workspace";
-
-interface WorkspaceIdPageProps {
-  params: {
-    workspaceId: string;
-  };
-}
-
 const WorkspaceIdPage = () => {
-  const workspaceId = useWorkspaceId();
-  const { data } = useGetWorkspace({ id: workspaceId });
-
   return (
     <div>
       <h1>Workspace ID Page</h1>
-      <p>{JSON.stringify(data)}</p>
     </div>
   );
 };
